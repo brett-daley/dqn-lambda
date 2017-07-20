bd12a902e05d15f6caec4ff936d6eac0b42b446d
Brett Daley <bdaley@ece.neu.edu>
2017-07-20 02:30:18 -0400

    Delete AgentDistilled.py

--- games/AgentDistilled.py
+++
@@ -1,8 +0,0 @@
-from GenericAgent import GenericAgent
-
-class AgentDistilled(GenericAgent):
-    def __init__(self, i_agt, dim_obs, n_actions):
-        super(self.__class__, self).__init__(i_agt = i_agt, dim_obs = dim_obs, n_actions = n_actions)
-
-    def get_obs(self):
-        pass
