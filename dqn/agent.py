f356bacc53d13495821d2aa68ec31a5e3e0aaa57
Brett Daley <bdaley@ece.neu.edu>
2017-11-08 16:53:08 -0500

    Define new class interfaces

---
+++ dqn/agent.py
@@ -0,0 +1,20 @@
+
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def random_policy(self):
+        pass
+
+    def epsilon_greedy_policy(self):
+        pass
+
+    def _get_random_action(self):
+        pass
+
+    def _get_greedy_action(self):
+        pass
+
+    def learn(self):
+        pass
---
+++ dqn/trainer.py
@@ -0,0 +1,11 @@
+
+
+class Trainer:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def benchmark(self):
+        pass
