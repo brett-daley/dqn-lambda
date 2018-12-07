yangyutu/dqn

Automate experiments

run_dqn_atari.py
@@ -1,5 +1,6 @@
 import gym
 import tensorflow as tf
+import argparse
 
 import dqn
 import utils
@@ -8,10 +9,20 @@ from q_functions import *
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env',         type=str,   default='PongNoFrameskip-v3')
+    parser.add_argument('--Lambda',      type=float, default=0.0)
+    parser.add_argument('--history_len', type=int,   default=4)
+    parser.add_argument('--recurrent',   action='store_true')
+    parser.add_argument('--video',       action='store_true')
+    args = parser.parse_args()
+
     session = utils.get_session()
 
-    env = gym.make(gym.benchmark_spec('Atari200M').tasks[3].env_id)
-    env = gym.wrappers.Monitor(env, 'videos/', force=True, video_callable=lambda e: False)
+    env = gym.make(args.env)
+    env = gym.wrappers.Monitor(env, 'videos/', force=True)
+    if not args.video:
+        env.video_callable = lambda e: False
     env = atari_wrappers.wrap_deepmind(env)
 
     seed = 0
@@ -26,9 +37,11 @@ def main():
                                outside_value=0.05,
                            )
 
+    q_func = AtariRecurrentConvNet() if args.recurrent else AtariConvNet()
+
     dqn.learn(
         env,
-        q_func=AtariConvNet(),
+        q_func,
         optimizer=optimizer,
         session=session,
         exploration=exploration_schedule,
@@ -36,10 +49,10 @@ def main():
         replay_buffer_size=1000000,
         batch_size=32,
         gamma=0.99,
-        Lambda=0.0,
+        Lambda=args.Lambda,
         learning_starts=50000,
         learning_freq=4,
-        history_len=4,
+        history_len=args.history_len,
         target_update_freq=10000,
         grad_clip=40.,
         log_every_n_steps=250000,

brett-daley/dqn-lambda

run_dqn_atari.py
@@ -1,5 +1,6 @@
 import gym
 import tensorflow as tf
+import argparse
 
 import dqn
 import utils
@@ -8,13 +9,24 @@ from q_functions import *
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env',         type=str,   default='PongNoFrameskip-v4')
+    parser.add_argument('--Lambda',      type=float, default=0.0)
+    parser.add_argument('--history_len', type=int,   default=4)
+    parser.add_argument('--seed',        type=int,   default=0)
+    parser.add_argument('--recurrent',   action='store_true')
+    parser.add_argument('--video',       action='store_true')
+    args = parser.parse_args()
+
     session = utils.get_session()
 
-    env = gym.make(gym.benchmark_spec('Atari200M').tasks[3].env_id)
-    env = gym.wrappers.Monitor(env, 'videos/', force=True, video_callable=lambda e: False)
+    env = gym.make(args.env)
+    env = gym.wrappers.Monitor(env, 'videos/', force=True)
+    if not args.video:
+        env.video_callable = lambda e: False
     env = atari_wrappers.wrap_deepmind(env)
 
-    seed = 0
+    seed = args.seed
     utils.set_global_seeds(seed)
     env.seed(seed)
 
@@ -26,9 +38,11 @@ def main():
                                outside_value=0.05,
                            )
 
+    q_func = AtariRecurrentConvNet() if args.recurrent else AtariConvNet()
+
     dqn.learn(
         env,
-        q_func=AtariConvNet(),
+        q_func,
         optimizer=optimizer,
         session=session,
         exploration=exploration_schedule,
@@ -36,10 +50,10 @@ def main():
         replay_buffer_size=1000000,
         batch_size=32,
         gamma=0.99,
-        Lambda=0.0,
+        Lambda=args.Lambda,
         learning_starts=50000,
         learning_freq=4,
-        history_len=4,
+        history_len=args.history_len,
         target_update_freq=10000,
         grad_clip=40.,
         log_every_n_steps=250000,