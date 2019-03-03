e65feff2c8cc30f7ad29a34808e6787306636f86
Brett Daley <brett-daley@users.noreply.github.com>
2019-03-03 20:32:43 +0800

    LSTMBlockFusedCell

--- q_functions.py
+++ q_functions.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.python.layers.layers import *
+from tensorflow.contrib.rnn import LSTMBlockFusedCell
 
 
 class QFunction:
@@ -40,15 +41,27 @@ class AtariRecurrentConvNet(QFunction):
             hidden = conv2d(hidden, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu)
             hidden = conv2d(hidden, filters=64, kernel_size=3, strides=1, activation=tf.nn.relu)
 
-            hidden = tf.reshape(hidden, [tf.shape(state)[0], state.shape[1], tf.size(hidden[0])])
+            batch_size = tf.shape(state)[0]
+            hidden = tf.reshape(hidden, [batch_size, state.shape[1], tf.size(hidden[0])])
 
-            cell = tf.contrib.rnn.BasicLSTMCell(num_units=512)
-            self.rnn_state = cell.zero_state(tf.shape(state)[0], tf.float32)
-            hidden, new_rnn_state = tf.nn.dynamic_rnn(cell, inputs=hidden, initial_state=self.rnn_state, dtype=tf.float32)
-
+            hidden, new_rnn_state = self.lstm(hidden, batch_size, num_units=512)
             qvalues = dense(hidden[:, -1], units=n_actions, activation=None)
 
         return qvalues, new_rnn_state
+
+    def lstm(self, inputs, batch_size, num_units, swap_axes=True):
+        if swap_axes:
+            inputs = tf.transpose(inputs, [1, 0, 2])
+        cell = LSTMBlockFusedCell(num_units)
+        self.rnn_state = self.zero_state(batch_size, num_units)
+        outputs, new_rnn_state = cell(inputs=inputs, initial_state=self.rnn_state, dtype=tf.float32)
+        if swap_axes:
+            outputs = tf.transpose(outputs, [1, 0, 2])
+        return outputs, new_rnn_state
+
+    def zero_state(self, batch_size, num_units):
+        shape = (batch_size, num_units)
+        return tuple([tf.zeros(shape), tf.zeros(shape)])
 
 
 class AtariConvNet(QFunction):
